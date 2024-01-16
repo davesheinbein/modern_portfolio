e96d090de63635ef8373fef7775c42863080b83a
David Sheinbein <davesheinbein@gmail.com>
2024-01-16 14:55:58 -0800

    update data

--- index.js
+++ index.js
@@ -216,6 +216,59 @@ window.addEventListener(
 	false
 );
 
+var jetpackSwiperLibraryPath = {
+	url: 'https://davidssoftwareengineer.wpcomstaging.com/wp-content/plugins/jetpack/_inc/build/carousel/swiper-bundle.min.js',
+};
+var jetpackCarouselStrings = {
+	widths: [370, 700, 1000, 1200, 1400, 2000],
+	is_logged_in: '1',
+	lang: 'en',
+	ajaxurl:
+		'https://davidssoftwareengineer.wpcomstaging.com/wp-admin/admin-ajax.php',
+	nonce: '3233a9f216',
+	display_exif: '1',
+	display_comments: '1',
+	single_image_gallery: '1',
+	single_image_gallery_media_file: '',
+	background_color: 'black',
+	comment: 'Comment',
+	post_comment: 'Post Comment',
+	write_comment: 'Write a Comment...',
+	loading_comments: 'Loading Comments...',
+	download_original:
+		'View full size <span class="photo-size">{0}<span class="photo-size-times">\u00d7</span>{1}</span>',
+	no_comment_text:
+		'Please be sure to submit some text with your comment.',
+	no_comment_email:
+		'Please provide an email address to comment.',
+	no_comment_author: 'Please provide your name to comment.',
+	comment_post_error:
+		'Sorry, but there was an error posting your comment. Please try again later.',
+	comment_approved: 'Your comment was approved.',
+	comment_unapproved: 'Your comment is in moderation.',
+	camera: 'Camera',
+	aperture: 'Aperture',
+	shutter_speed: 'Shutter Speed',
+	focal_length: 'Focal Length',
+	copyright: 'Copyright',
+	comment_registration: '0',
+	require_name_email: '1',
+	login_url:
+		'https://davidssoftwareengineer.wpcomstaging.com/wp-login.php?redirect_to=https%3A%2F%2Fdavidssoftwareengineer.wpcomstaging.com%2F2024%2F01%2F10%2Fearly-insights-on-chatgpt-a-chatbot-technology-with-great-potential%2F',
+	blog_id: '1',
+	meta_data: [
+		'camera',
+		'aperture',
+		'shutter_speed',
+		'focal_length',
+		'copyright',
+	],
+};
+
+var WPGroHo = {
+	my_hash: '608846e8c39caf645a7ed901ebcee02a',
+};
+
 (function () {
 	var request,
 		b = document.body,
